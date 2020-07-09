5b1e3fadc902acbdd1143918c67820c011adf887
Oleg Kupavtsev <olegkupavtsev@gmail.com>
2020-07-09 20:28:01 +0300

    some more for objects and arrays

--- Objects/spreadDestruction.js
+++ Objects/spreadDestruction.js
@@ -53,3 +53,38 @@ let {width:w=100, height:h=200, title} = options;
 alert(title);  // Меню
 alert(w);      // 100
 alert(h);      // 200
+
+'use strict';
+
+let block = {
+  width: 170,
+  height: 90,
+  price: 290,
+  type: 'solid'
+}
+
+let {width, height, ...rest} = block;
+
+
+'use strict';
+let block = {
+  width: 170,
+  height: {
+    value: 50,
+    unit: 'cm'
+  }
+}
+
+let {
+  width,
+  height: {value, unit}
+} = block;
+
+
+'use strict';
+let person = {
+  name: 'Sam',
+  years: 25
+}
+
+let { name, years: age, height=null } = person;
